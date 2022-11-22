e42b7a789fcd736595a15213f7bf7a86097c3395
manhhaa <hanguyenns793@gmail.com>
2022-11-22 21:23:35 +0700

    #17

--- src/route/web.js
+++ src/route/web.js
@@ -1,14 +1,13 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("hello");
-  });
+  router.get("/", homeController.getHomePage);
+  router.get("/about", homeController.getAboutPage);
+
   return app.use("/", router);
 };
 
-module.exports = {
-  initWebRoutes,
-};
+module.exports = initWebRoutes;
