1ec76f853259d203bfd829157d1a4b7ee52429a9
manhhaa <hanguyenns793@gmail.com>
2022-11-23 15:43:43 +0700

    #23

--- src/seeders/seeder-demo-user.js
+++ src/seeders/seeder-demo-user.js
@@ -4,9 +4,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "admin@gmail.com",
+        password: "123123",
+        firstName: "Ha ",
+        lastName: "Cac",
+        address: "Ha Nam",
+        gender: "1",
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
